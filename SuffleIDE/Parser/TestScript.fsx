6b6413496b0311b08d96f8c3702189357f71896b
Alex Semin <Alllex.Semin@gmail.com>
2014-03-27 20:00:33 +0400

    Add TestScript

---
+++ SuffleIDE/Parser/TestScript.fsx
@@ -0,0 +1,11 @@
+﻿
+#r @"M:\projects\Suffle\SuffleIDE\Parser\bin\Debug\Types.dll"
+#r @"M:\projects\Suffle\SuffleIDE\Parser\bin\Debug\ParserCombinator.dll"
+#load "Auxiliary.fs"
+#load "Literals.fs"
+#load "Binary.fs"
+#load "Types.fs"
+
+open ParserCombinators.Core
+open Types
+open Parser.Types
