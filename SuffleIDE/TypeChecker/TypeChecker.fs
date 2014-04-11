df34fb92cb9aa973ac62e228dc51db1eb9695296
Alex Semin <Alllex.Semin@gmail.com>
2014-04-11 13:35:34 +0500

    Add TypeContext class

--- SuffleIDE/TypeChecker/TypeChecker.fs
+++ SuffleIDE/TypeChecker/TypeChecker.fs
@@ -1,2 +1,26 @@
 ﻿module Suffle.TypeChecker
 
+open Suffle.Specification.Types
+
+type TypeContextItem = EIdent * Type
+
+type TypeContext(ctx : TypeContextItem list) =
+  class
+    member x.Add(p : TypeContextItem) = 
+        new TypeContext(p :: ctx)
+
+    member x.Lookup(i : EIdent) =
+        List.tryFind (fun (i' : EIdent, _) -> i'.Name = i.Name) ctx
+  end
+
+type TypeCheckerResult = bool * string
+
+let typeValue tctx =
+    function
+    | VUnit    -> TUnit
+    | VBool _  -> TBool
+    | VChar _  -> TChar
+    | VInt _   -> TInt
+    | VCons(name, _) -> TUnit
+    | VClosure(ctx, e) -> TUnit
+    | _ -> failwith "Pattern match faliure"
