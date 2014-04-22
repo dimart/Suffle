0bda77114fdc53cb2784d1145642cc79693027d6
CepGamer <blossom120@gmail.com>
2014-04-22 23:56:51 +0400

    Added almost all atomic expression tests

--- SuffleIDE/Interpreter/Expression.fs
+++ SuffleIDE/Interpreter/Expression.fs
@@ -6,13 +6,12 @@ open Interpreter.ExceptionList
 
 let lineNum = 0
 
-let vars = new Dictionary<string, Stack<Value>>()
-let funcs = new Dictionary<string, Stack<DFunction>>()
+type Interpreter () = 
+    let vars = new Dictionary<string, Stack<Value>>()
+    let funcs = new Dictionary<string, Stack<DFunction>>()
 
-//  Evaluate expression
-let rec evalExpr (expr: Expression) = 
     //  Return binary operator
-    let retNumBin (op: BinaryOp): int -> int -> int = 
+    let rec retNumBin (op: BinaryOp): int -> int -> int = 
         match op with
         | BAdd -> (+)
         | BSub -> (-)
@@ -20,7 +19,7 @@ let rec evalExpr (expr: Expression) =
         | BMul -> (*)
         | _ -> raise (TypeMismatchException ("Wrong operation", lineNum))
 
-    let retBoolNumBin (op: BinaryOp): int -> int -> bool = 
+    and retBoolNumBin (op: BinaryOp): int -> int -> bool = 
         match op with
         | BEQ -> (=)
         | BNEQ -> (<>)
@@ -30,7 +29,7 @@ let rec evalExpr (expr: Expression) =
         | BNLT -> (>=)
         | _ -> raise (TypeMismatchException ("Wrong operation", lineNum))
 
-    let retBoolBin (op: BinaryOp): bool -> bool -> bool = 
+    and retBoolBin (op: BinaryOp): bool -> bool -> bool = 
         match op with
         | BAnd -> (&&)
         | BOr -> (||)
@@ -38,13 +37,13 @@ let rec evalExpr (expr: Expression) =
 
     //  Evaluate type expression
     //  ETyped
-    let evalType (x: Expression) = 
+    and evalType (x: Expression) = 
         //toWrite
         VUnit
 
     //  Eval identifier
     //  EIdentifier
-    let evalIdent (id: EIdent) = 
+    and evalIdent (id: EIdent) = 
 //            try
         try 
             (vars.Item id.Name).Peek()
@@ -56,17 +55,17 @@ let rec evalExpr (expr: Expression) =
 //                | VariableNotFoundException name -> printfn "Variable \"%A\" not found" name
 
     //  Eval literal
-    let evalLiteral (liter: ELiteral) = 
+    and evalLiteral (liter: ELiteral) = 
         liter.Value
         
     //  Eval "if" statement 
-    let evalIf (stmnt: EIfElse) = 
+    and evalIf (stmnt: EIfElse) = 
         match (evalExpr stmnt.Cond) with
         | VBool cond -> evalExpr (if cond then stmnt.OnTrue else stmnt.OnFalse)
         | _ -> raise (TypeMismatchException ("Expected type \'bool\'.", lineNum))
         
     //  Eval "let" stmnt
-    let evalLet (stmnt: ELetIn) = 
+    and evalLet (stmnt: ELetIn) = 
         match stmnt.Binding with
         | DValue x -> (vars.Item x.Name.Name).Push <| evalExpr x.Value
         //  May be need to pass closure or lambda - dunno now, check on it later
@@ -75,7 +74,7 @@ let rec evalExpr (expr: Expression) =
         evalExpr stmnt.Body
             
     //  Eval unary stmnt
-    let evalUnary (stmnt: EUnary) = 
+    and evalUnary (stmnt: EUnary) = 
         let evaluated = evalExpr stmnt.Arg
         match stmnt.Op with
         | UNeg ->   match evaluated with
@@ -86,7 +85,7 @@ let rec evalExpr (expr: Expression) =
                     | _ -> raise (TypeMismatchException ("Bool expected", lineNum))
 
     //  Eval binary statement
-    let evalBinary (stmnt: EBinary) = 
+    and evalBinary (stmnt: EBinary) = 
         let evaluated1 = evalExpr stmnt.Arg1
         let evaluated2 = evalExpr stmnt.Arg2
         match evaluated1 with
@@ -95,24 +94,32 @@ let rec evalExpr (expr: Expression) =
                     | VInt y -> VInt <| retNumBin stmnt.Op x y
                     | _ -> raise (TypeMismatchException ("Int expected", lineNum))
         | VBool x -> match evaluated2 with
-                     | VBool y -> VBool<| retBoolBin stmnt.Op x y
-                     | _ -> raise (TypeMismatchException ("Int expected", lineNum))
+                        | VBool y -> VBool<| retBoolBin stmnt.Op x y
+                        | _ -> raise (TypeMismatchException ("Int expected", lineNum))
         | _ -> raise (TypeMismatchException ("Int expected", lineNum))
-                            
-(*                            
-| ELambda of ELambda
-| EFunApp of EFunApp
-| ECaseOf of ECaseOf
-*)
 
-    match expr with
-    | EIdent x -> evalIdent x
-    | ELiteral x -> evalLiteral x
-    | EIfElse x -> evalIf x
-    | ELetIn x -> evalLet x
-    | EBinary x -> evalBinary x
-    | EUnary x -> evalUnary x
-    | _ -> failwith "Not Implemented Pattern"
+    and evalLambda (stmnt: ELambda) = 
+        match stmnt.Arg with
+        | 
+
+    //  Evaluate expression
+    and evalExpr (expr: Expression) = 
+                            
+    (*                            
+    | ELambda of ELambda
+    | EFunApp of EFunApp
+    | ECaseOf of ECaseOf
+    *)
+
+        match expr with
+        | EIdent x -> evalIdent x
+        | ELiteral x -> evalLiteral x
+        | EIfElse x -> evalIf x
+        | ELetIn x -> evalLet x
+        | EBinary x -> evalBinary x
+        | EUnary x -> evalUnary x
+        | ELambda x -> evalLambda x
+        | _ -> failwith "Not Implemented Pattern"
     
 let a = Expr (EBinary {Op = BAdd; Arg1 = ELiteral {Value = VInt 5}; Arg2 = ELiteral {Value = VInt 7}})
 //eval a Map.empty Map.empty
