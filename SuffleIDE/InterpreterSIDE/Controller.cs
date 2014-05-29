a30a68abd511866518a67958e91f1b19682ee1f2
dimart <dimart.sp@gmail.com>
2014-05-29 23:12:04 +0400

    Create Suffle interpreter module for SIDE

---
+++ SuffleIDE/InterpreterSIDE/Controller.cs
@@ -0,0 +1,16 @@
+﻿using Suffle.Interpreter;
+using Parser;
+
+using Side.Interfaces.Services;
+
+namespace InterpreterSIDE
+{
+    public class Controller : IInterpreter
+    {
+        public string Interpret(string code)
+        {
+            var inter = new Suffle.Interpreter.Expression.Interpreter();
+            return inter.EvaluateProgram(Suffle.Parser.parse(code));
+        }
+    }
+}
---
+++ SuffleIDE/InterpreterSIDE/Interpreter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Side.Interfaces.Services;
+
+namespace InterpreterSIDE
+{
+    [Module(ModuleName = "Side.Interpreter")]
+    public class Interpreter : IModule
+    {
+        private IUnityContainer _container;
+
+        public Interpreter(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        #region IModule Members
+
+        public void Initialize()
+        {
+            _container.RegisterType<IInterpreter, Controller>(new ContainerControlledLifetimeManager());
+        }
+
+        #endregion
+    }
+}
